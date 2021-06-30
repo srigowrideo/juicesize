9dba6738bb191754f21eb86c819ab47a93b31073
Srigowri Deogirikar <32318383+srigowrideo@users.noreply.github.com>
2021-06-30 12:49:34 +0530

    Add files via upload

---
+++ freshjuice.java
@@ -0,0 +1,22 @@
+
+package fibonacciseries;
+ public class freshjuice{
+	enum level
+	{
+		SMALL,
+		MEDIUM,
+		LARGE}
+
+	public String size;
+		
+	
+ }
+
+
+	public static void main(String[] args) {
+	freshjuice juice=new freshjuice();
+	juice.size=freshjuice.level.MEDIUM;
+System.out.println("SIZE"+juice.size);
+	}
+
+
